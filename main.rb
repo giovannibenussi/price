39b5507ba8e79d9607c600b1cc22b3cae7623c95
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-12-21 18:13:33 -0300

    Read URLs from urls.yml

--- main.rb
+++ main.rb
@@ -1,14 +1,8 @@
+require 'yaml'
 require_relative 'lib/price.rb'
 
-urls = [
-    "https://www.pcfactory.cl/producto/23898-Smartphone.Galaxy.J7.Prime.Octa.Core.16GB.55.FHD.4G.Android.6.0.Negro.Liberado",
-    # "http://www.falabella.com/falabella-cl/product/5358900/Smartphone-Galaxy-J7-Prime-Negro-Dual-Sim-Entel"
-]
-
-# amazon = Price::Falabella.new("http://www.falabella.com/falabella-cl/product/5358900/Smartphone-Galaxy-J7-Prime-Negro-Dual-Sim-Entel")
-# puts Price::Falabella.name
-# puts amazon.price
-# puts amazon.price
+urls = YAML.load_file('urls.yml')
+puts urls.to_s
 
 urls.each do | url |
     product = Price.from_url(url)
