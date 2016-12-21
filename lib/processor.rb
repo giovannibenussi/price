10d6e08fb40882d9a3a7546c8b3f04bb9f5e847c
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-12-21 01:20:20 -0300

    Remove get method to price instance

--- lib/processor.rb
+++ lib/processor.rb
@@ -5,9 +5,6 @@ require 'open-uri'
 class Processor
     def initialize(url)
         @url = url
-    end
-
-    def get
         @doc = Nokogiri::HTML(open(@url))
     end
 end
--- main.rb
+++ main.rb
@@ -1,5 +1,4 @@
 require_relative 'lib/price.rb'
 
 amazon = Price::Falabella.new("http://www.falabella.com/falabella-cl/product/5358900/Smartphone-Galaxy-J7-Prime-Negro-Dual-Sim-Entel")
-amazon.get
 puts amazon.price
