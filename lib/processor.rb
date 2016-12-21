903ac7a21ad8a1db535e9d03c4a53b247904bf7d
Giovanni Benussi <giovanni.benussi@usach.cl>
2016-12-21 18:59:10 -0300

    Class attributes defined as singleton methods for better readability

--- lib/processor.rb
+++ lib/processor.rb
@@ -12,6 +12,17 @@ class Processor
     end
 
     class << self
-        attr_reader :name, :host
+        def pretty_setter(*attrs)
+            [*attrs].each do | attr |
+                attr = attr.to_s
+                define_singleton_method(attr) do | value = nil |
+                    return instance_variable_get("@#{attr}") if value.nil?
+                    instance_variable_set("@#{attr}", value)
+                end
+            end
+        end
     end
+
+    attr_writer :name, :host
+    pretty_setter :name, :host
 end
